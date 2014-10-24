0e3b81c0a708e1ef13e85c70782f1779eefa985c
Corinn Herrick <herrickc@mit.edu>
2014-10-24 03:59:44 -0400

    Created the basic models.

--- PomodoroTracker/pomodoros/models.py
+++ PomodoroTracker/pomodoros/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Pomodoro(models.Model):
+    """ Represents a 25 minute unit of work.
+
+        Fields:
+            * tasks (optional) -- the tasks associated with this pomodoro. Many pomodoros
+                                  may be associated with the same task and many tasks may
+                                  be associated with the same pomodoro.
+            * day (required) -- the day when this pomodoro will be completed.
+            * completed (default=False) -- whether the pomodoro has been completed
+    """
+    tasks = models.ManyToManyField('tasks.Task', blank=True)
+    day = models.ForeignKey('Day')
+    completed = models.BooleanField(default=False)
+
+class Day(models.Model):
+    """ Represents a day of work for the user.
+    
+        Fields:
+            * date (required) -- the date of the day.
+    """
+    date = models.DateField()
+
+class Distraction(models.Model):
+    """ Represents a distraction a user encounters during a pomodoro.
+
+        Fields:
+            * body (required) -- a short (500 char or less) description of the distraction
+            * pomodoro (required) -- the pomodoro the distraction occurred during
+            * status[NOT_HANDLED, TASK, IGNORE] (default=NOT_HANDLED) -- what the status
+                                                of dealing with the distraction is.
+    """
+    body = models.CharField(max_length=500)
+    pomodoro = models.ForeignKey('Pomodoro')
+
+    NOT_HANDLED = 0
+    TASK = 1
+    IGNORE = 2
+    STATUS_OPTIONS = (
+        (NOT_HANDLED, 'To be dealt with.')
+        (TASK, 'Became a task.'),
+        (INGORE, 'Ignored.'),
+    )
+    status = models.SmallPositiveIntegerField(choices=STATUS_OPTIONS, default=NOT_HANDLED)
--- PomodoroTracker/tasks/models.py
+++ PomodoroTracker/tasks/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    """ Represents a single task that the user needs to accomplish.
+
+        Fields:
+            * body (required) -- a short (500 char or less) description of the task.
+            * project (optional) -- the project model that the task is associated with.
+                         If blank, then project is in inbox.
+            * day (optional) -- the day model that the task is assigned to be done during.
+            * due_date (optional) -- day when the task is due
+            * importance[LOW, MEDIUM, HIGH] (default=MEDIUM) -- the importance of a task 
+                                                                (distinct from its  urgency)
+            * need_to_happen (default=True) -- whether the task needs to be done or is optional
+            * expected_pomodoros (default=1) -- how many pomodoros the task is expected to take
+            * actual_pomodoros (default=0) -- how many pomodoros the task has taken
+            * completed (default=False) -- whether the task has been completed
+
+       Related Models:
+            * Pomodoro -- a many to many relationship
+    """
+    body = models.CharField(max_length=500)
+    project = models.ForeignKey('Project', blank=True)
+    day = models.ForeignKey('pomodoros.Day', blank=True)
+    due_date = models.DateField(blank=True)
+
+    LOW= 1
+    MEDIUM = 2
+    HIGH = 3
+    IMPORTANCE_CHOICES = (
+        (LOW, 'Not Important'),
+        (MEDIUM, 'Somewhat Important'),
+        (HIGH, 'Very Important'),
+    )
+    importance = models.PositiveSmallIntegerField(choices=IMPORTANCE_CHOICES, default=2)
+    need_to_happen = models.BooleanField(default=True)
+    expected_pomodoros = models.PositiveSmallIntegerField(default=1)
+    actual_pomodoros = models.PositiveSmallIntegerField(default=0)
+    completed = models.BooleanField(default=False)
+
+
+class Project(models.Model):
+    """ Represents a project, an organizational structure for tasks.
+
+        Fields:
+            * title (required) -- a short (100 char or less) title for the project
+    """
+    title = models.CharField(max_length = 100)
